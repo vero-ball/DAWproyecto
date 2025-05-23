fe1a8b40107004cb9dc53c519ab125900227f9a1
vero-ball <v.rivadulla.deschamps@gmai.com>
2025-05-23 18:57:12 +0200

    Commit inicial: frontend Angular 19 + backend Node.js

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Conectado a MongoDB'))
+.catch(err => console.error('Error al conectar a MongoDB:', err));
+
+app.post('/upload', upload.single('archivo'), (req, res) => {
+  console.log(req.file);
+  res.send('Archivo subido');
+});
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
+});
