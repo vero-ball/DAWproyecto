cd9be34f504b013f6915871d901830e58e382dfd
vero-ball <v.rivadulla.deschamps@gmai.com>
2025-06-03 17:44:32 +0200

    paleta de colores css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2A6ACB",    // Azul principal
+        secondary: "#F9D94A",  // Amarelo principal
+        accent: "#E6B800",     // Dourado
+        success: "#4CA44C",    // Verde
+        error: "#E23D3D",      // Vermello
+        dark: "#222222",       // Negro
+        light: "#FFFFFF",      // Branco
+      }
+    }
   },
   plugins: [],
 }
